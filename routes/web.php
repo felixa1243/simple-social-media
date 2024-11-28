ee77aa380569b44e1bbd3d55a37fce47480347bf
Rajiph iqbal <felixarajiph@gmail.com>
2024-11-28 21:12:38 +0700

    feat: add prototype profile view

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\MediaController;
+use App\Livewire\PublicUserProfile;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-
+Route::get("user", PublicUserProfile::class);
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
